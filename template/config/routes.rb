aa1fe0e9eb0e1ba666c25dd4533dcc0ecaebffc0
Brian2524 <b2hinkle@gmail.com>
2020-03-17 02:04:39 +0000

    add the information page

--- template/config/routes.rb
+++ template/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/new'
   root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
+  get  '/information',    to: 'static_pages#information'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
