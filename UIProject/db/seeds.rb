775f70338d4d748116cd0d0610ad229685cd7303
Brian2524 <b2hinkle@gmail.com>
2020-04-23 20:06:42 +0000

    Fixed delete user buttton, and added an admin user to the seeds.rb

--- UIProject/db/seeds.rb
+++ UIProject/db/seeds.rb
@@ -1,17 +1,17 @@
 # Create a main sample user.
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "AdminUser",
+             email: "AdminUser@AdminUser.com",
+             password:              "AdminUser",
+             password_confirmation: "AdminUser",
              admin: true)
 
 # Generate a bunch of additional users.
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end 
+# 99.times do |n|
+#   name  = Faker::Name.name
+#   email = "example-#{n+1}@railstutorial.org"
+#   password = "password"
+#   User.create!(name:  name,
+#               email: email,
+#               password:              password,
+#               password_confirmation: password)
+# end 
