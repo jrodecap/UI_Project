960677aeee1d295132e4a2555d8edbe7979a58db
Jordan Rodecap <jarodecap@csustudent.net>
2020-04-24 01:55:43 +0000

    test .js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("jquery")
+// require("@rails/ujs").start()
+// require("turbolinks").start()
+// require("@rails/activestorage").start()
+// require("channels")
+// require("jquery")
+// import "bootstrap"
+require ("jquery")
+require ("jquery_ujs")
+require ("turbolinks")
+require ("bootstrap")
 import "bootstrap"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
